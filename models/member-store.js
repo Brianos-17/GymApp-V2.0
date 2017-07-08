966d9d3d515597c986e292f0c39b8707e09fd4f0
Brian <brianos-17git@hotmail.com>
2017-07-08 19:01:25 +0100

    added login and signup html

--- models/member-store.js
+++ models/member-store.js
@@ -20,7 +20,7 @@ const memberStore = {
   },
 
   getMemberById(id) {
-    return this.store.findOneBy(this.collection, { id: id});
+    return this.store.findOneBy(this.collection, { id: id });
   },
 
   getMemberByEmail(email) {
