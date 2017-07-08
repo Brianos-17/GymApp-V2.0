fce87250071b385038c9927d62d95361eaa9619e
Brian <brianos-17git@hotmail.com>
2017-07-08 19:41:14 +0100

    began work on routes.js

---
+++ routes.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Brian on 08/07/2017.
+ */
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const accounts = require('./controllers/accounts.js');
+const dashboard = require('./controllers/dashboard.js');
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
