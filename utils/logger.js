dd427b44a68d03a5adf73e17dab71150179e9854
Brian <brianos-17git@hotmail.com>
2017-07-08 12:56:25 +0100

    Introduced logger.js to allow for acurate debugging and information

---
+++ utils/logger.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Brian on 08/07/2017.
+ */
+const winston = require('winston');
+
+const logger = new (winston.Logger)({
+  transports: [new (winston.transports.Console)({ json: true })],
+});
+
+logger.level = 'debug';
+
+if (process.env.LEVEL) {
+  logger.level = process.env.LEVEL;
+}
+
+module.exports = logger;
