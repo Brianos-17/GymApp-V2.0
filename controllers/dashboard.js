636dbdd5116d5693942b5fec4f72ff455cce0094
Brian <brianos-17git@hotmail.com>
2017-07-08 14:02:06 +0100

    Added member-store.js, json-store.js and a JSON file to store data

---
+++ controllers/dashboard.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Brian on 08/07/2017.
+ */
+'use strict';
+
+const logger = require('../utils/logger.js');
+
+const dashboard = {
+  index(request, response) {
+    logger.info('rendering dashboard');
+  }
+}
