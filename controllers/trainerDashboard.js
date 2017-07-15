75351e3d2eb2b248aaa4414880ef42d65c6b8bd5
Brian <brianos-17git@hotmail.com>
2017-07-15 13:18:42 +0100

    began work on trainer dashboard

--- controllers/trainerDashboard.js
+++ controllers/trainerDashboard.js
@@ -1,3 +1,14 @@
 /**
  * Created by Brian on 15/07/2017.
  */
+'use strict';
+
+const logger = require('../utils/logger.js');
+const accounts = require('./accounts.js');
+
+const trainerDashboard = {
+  index(request, response) {
+    logger.info('rendering trainer dashboard');
+    const loggedInTrainer = accounts.getCurrentTrainer(request);
+  }
+}
