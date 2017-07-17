feabff0da000d8fcb5ba287807fea39037d4b115
Brian <brianos-17git@hotmail.com>
2017-07-17 14:21:34 +0100

    changed members-store.json to .js in trainer dashboard, renamed const members to allMembers, renamed appropriate each loop in trainerDashboard.hbs

--- controllers/trainerDashboard.js
+++ controllers/trainerDashboard.js
@@ -5,27 +5,28 @@
 
 const logger = require('../utils/logger.js');
 const accounts = require('./accounts.js');
-const members = require('../models/members-store.json');
+const allMembers = require('../models/members-store.js');
 const trainer = require('../models/trainer-store.js');
 
 const trainerDashboard = {
   index(request, response) {
     const loggedInTrainer = accounts.getCurrentTrainer(request);
+    const memberList = allMembers.getAllMembers();
     const viewData = {
       title: 'Trainer Dashboard',
       trainer: loggedInTrainer,
-      members: [],
+      memberList: memberList,
     };
     logger.info(`rendering trainer dashboard for ${loggedInTrainer.firstName}`);
     response.render('trainerDashboard', viewData);
   },
 
-  addMemberList(request) {
-    const loggedInTrainer = accounts.getCurrentTrainer(request);
-    const id = loggedInTrainer.id;
-    const members = members;
-    trainer.addMemberList(id, members);
-  },
+  // addMemberList(request) {
+  //   const loggedInTrainer = accounts.getCurrentTrainer(request);
+  //   const id = loggedInTrainer.id;
+  //   const members = members;
+  //   trainer.addMemberList(id, members);
+  // },
 };
 
 module.exports = trainerDashboard;
