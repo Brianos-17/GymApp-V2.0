59767a258017f9c8bc8b3d1c4a31e5b4143db6e9
Brian <brianos-17git@hotmail.com>
2017-07-21 15:25:32 +0100

    added updateFirstName method in accounts.js and route to support this

--- routes.js
+++ routes.js
@@ -28,5 +28,6 @@ router.post('/register', accounts.register);
 router.post('/authenticate', accounts.authenticate);
 router.post('/dashboard/addassessment', dashboard.addAssessment);
 router.post('/memberAssessment/:id/updateComment/:assessmentId', trainerDashboard.updateComment);
+router.post('/updateProfileFirstName', accounts.updateProfileFirstName);
 
 module.exports = router;
